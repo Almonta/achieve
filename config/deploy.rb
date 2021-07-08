48dfdb3fec305a306e030ecc52710cc717a1ce54
motohiro takagi <tkg.8.mthr@gmail.com>
2021-07-08 11:53:33 +0900

    init unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock '3.6.0'
 set :application, 'achieve'
 # cloneするgitのレポジトリ
 # （xxxxxxxx：ユーザ名、yyyyyyyy：アプリケーション名）
-set :repo_url, 'https://github.com/Almonta/210708_AWS.git'
+set :repo_url, 'git@github.com:Almonta/achieve.git'#'https://github.com/Almonta/210708_AWS.git'
 # deployするブランチ。デフォルトでmainを使用している場合、masterをmainに変更してください。
 set :branch, ENV['BRANCH'] || 'master'
 # deploy先のディレクトリ。
